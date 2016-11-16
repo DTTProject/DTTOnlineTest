dd3f36ab89c5f9c4f900d853dffe363d3b3d40b2
Harry Nguyen <hamxvela@gmail.com>
2016-11-16 10:56:24 +0700

    1740_1741_1742_1743_manage_course

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   namespace :admin do
     root "users#index"
     resources :users
+    resources :courses
   end
+  post '/admin/courses' => 'admin/courses#create', as: :courses
+  put '/admin/courses/:id' => 'admin/courses#update', as: :course
 end
