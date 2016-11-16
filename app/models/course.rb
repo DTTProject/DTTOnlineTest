f278ea920d90cedc4934a3755b84fc494165e3d7
Harry Nguyen <hamxvela@gmail.com>
2016-11-16 10:56:24 +0700

    1740_1741_1742_1743_manage_course

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,5 @@ class Course < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :tests, dependent: :destroy
   has_many :comments, dependent: :destroy
+  mount_uploader :image, ImageUploader
 end
