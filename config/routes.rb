5af4c82ae4d209aa151721b3cbdc5a9eeb4e2fad
Tao Nhu <tao.nhu@sugar.sg>
2016-11-25 11:19:03 +0700

    implement facebook sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "homepages#index"
   devise_for :users, controllers: {registrations: "users/registrations",
-    sessions: "users/sessions"}
+    sessions: "users/sessions", :omniauth_callbacks => "users/omniauth_callbacks"}
   namespace :admin do
     root "users#log"
     resources :courses
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
     end
     resources :questions
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161124122813) do
+ActiveRecord::Schema.define(version: 20161125040557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +110,8 @@ ActiveRecord::Schema.define(version: 20161124122813) do
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
     t.datetime "deleted_at"
+    t.string   "provider"
+    t.string   "uid"
     t.index ["deleted_at"], name: "index_users_on_deleted_at", using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
